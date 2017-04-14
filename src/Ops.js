1553e9b1270219fc37e53326ee5abe02c5762335
yankechao <yankechao@cloudin.cn>
2017-04-15 00:21:20 +0800

    完成总览页和运维页面路由搭建

---
+++ src/Ops.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {Layout, Breadcrumb} from 'antd';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import TicketView from './component/TicketView'
+const {Sider, Content} = Layout;
+class Ops extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const {match} = this.props;
+    console.log(match);
+    return (
+      <Router>
+        <Layout>
+          <Sider style={{background: '#aaa'}}>
+            <ul>
+              <li><Link to={match.url + "/ticketView:monitor"}>监控工单</Link></li>
+              <li><Link to={match.url + "/ticketView:breakdown"}>报障工单</Link></li>
+              <li><Link to={match.url + "/ticketView:service"}>服务工单</Link></li>
+              <li><Link to={match.url + "/ticketView:CMDB"}>CMDB</Link></li>
+            </ul>
+          </Sider>
+          <div>
+            {this.props.children}
+          </div>
+          <Route path={match.url + "/ticketView:id"} component={TicketView}/>
+        </Layout>
+      </Router>
+    );
+  }
+}
+
+export default Ops;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Ops from './Ops'
+import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {Layout} from 'antd';
 import './index.css';
 
+
+const {Header} = Layout;
+var Config = () => (
+  <Router>
+    <Layout>
+      <Header style={{display: 'flex'}}>
+        <Link to="/">首页</Link>
+        <Link to="/ops">运维</Link>
+        <Link to="/monitor">监控</Link>
+        <Link to="/ticket">工单</Link>
+        <img src={logo} alt="" className="app-logo"/>
+      </Header>
+      <Route exact path='/' component={App}/>
+      <Route exact path='/ops' component={Ops}/>
+      <Route exact path='/monitor' component={App}/>
+      <Route exact path='/ticket' component={App}/>
+    </Layout>
+  </Router>
+);
+
 ReactDOM.render(
-  <App />,
+  <Config />,
   document.getElementById('root')
 );
